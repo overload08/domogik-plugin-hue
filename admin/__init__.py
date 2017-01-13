eb200217e010d2692f2a6e4002dd42c09f0b5ef2
root <root@domogik>
2017-01-13 20:15:38 +0100

    Correct errors

--- admin/__init__.py
+++ admin/__init__.py
@@ -15,13 +15,16 @@ import subprocess
 
 ### package specific functions
 def list_lights():
-    b=Bridge()
+    b=Bridge(self.get_config("ip_bridge"))
     b.connect()
     output = b.get_light()
+    if isinstance(output, str):
+        output = unicode(output, 'utf-8')
     return output
 
+
 ### common tasks
-package = "plugin_bluez"
+package = "plugin_hue"
 template_dir = "{0}/{1}/admin/templates".format(get_packages_directory(), package)
 static_dir = "{0}/{1}/admin/static".format(get_packages_directory(), package)
 
@@ -29,17 +32,16 @@ plugin_hue_adm = Blueprint(package, __name__,
                         template_folder = template_dir,
                         static_folder = static_dir)
 
-@plugin_hue_adm.route('/<client_id')
+
+@plugin_hue_adm.route('/<client_id>')
 def index(client_id):
     detail = get_client_detail(client_id)
     try:
-	return render_template('plugin_hue.html',
-	    clientid = client_id,
-	    client_detail = detail,
-	    mactive = "clients",
-	    active = 'advanced',
-	    hue = list_lights())
-
+        return render_template('plugin_hue.html',
+            clientid = client_id,
+            client_detail = detail,
+            mactive="clients",
+            active = 'advanced')
     except TemplateNotFound:
         abort(404)
 
