6e3f93c6020614065f71fdcc31fe8ed69a5ba935
root <root@domogik>
2017-01-13 19:09:41 +0100

    Added admin page

---
+++ admin/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+### common imports
+from flask import Blueprint, abort
+from domogik.common.utils import get_packages_directory
+from domogik.admin.application import render_template
+from domogik.admin.views.clients import get_client_detail
+from jinja2 import TemplateNotFound
+from phue import Bridge
+
+### package specific imports
+import subprocess
+
+
+
+### package specific functions
+def list_lights():
+    b=Bridge()
+    b.connect()
+    output = b.get_light()
+    return output
+
+### common tasks
+package = "plugin_bluez"
+template_dir = "{0}/{1}/admin/templates".format(get_packages_directory(), package)
+static_dir = "{0}/{1}/admin/static".format(get_packages_directory(), package)
+
+plugin_hue_adm = Blueprint(package, __name__,
+                        template_folder = template_dir,
+                        static_folder = static_dir)
+
+@plugin_hue_adm.route('/<client_id')
+def index(client_id):
+    detail = get_client_detail(client_id)
+    try:
+	return render_template('plugin_hue.html',
+	    clientid = client_id,
+	    client_detail = detail,
+	    mactive = "clients",
+	    active = 'advanced',
+	    hue = list_lights())
+
+    except TemplateNotFound:
+        abort(404)
+
